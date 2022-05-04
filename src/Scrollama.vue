eccadc74399aa7f47a5d97392e7934272522acda
timuism <timuism@me.com>
2022-05-04 11:17:11 -0400

    Add vue 3 support, switch to composition api

--- src/Scrollama.vue
+++ src/Scrollama.vue
@@ -1,51 +1,49 @@
 <template>
-  <div class="scrollama__steps">
-    <slot />
-  </div>
+    <div ref="el" class="scrollama__steps">
+        <slot />
+    </div>
 </template>
 
-<script>
-import scrollama from 'scrollama';
+<script setup lang="ts">
+import scrollama, { ScrollamaInstance, ScrollamaOptions } from 'scrollama';
+import { computed, ref, Ref, onBeforeUnmount, onMounted, defineEmits, useAttrs } from 'vue';
 
-export default {
-  inheritAttrs: false,
-  name: 'Scrollama',
-  mounted () {
-    this._scroller = scrollama();
-    this.setup();
-  },
-  beforeDestroy() {
-    this._scroller.destroy();
-  },
-  computed: {
-    opts() {
-      return Object.assign({},  {
-        step: Array.from(this.$el.children),
-        progress: !!this.$listeners['step-progress']
-      }, this.$attrs);
-    }
-  },
-  methods: {
-    setup() {
-      this._scroller.destroy();
+let scroller:ScrollamaInstance = scrollama();
+const attrs = useAttrs()
+const emit = defineEmits(['step-progress', 'step-enter', 'step-exit'])
+const el: Ref<HTMLDivElement | null> = ref(null)
+const opts = computed(() => {
+    let rootElem = el.value as HTMLDivElement
+    return Object.assign({}, {
+        step: Array.from(rootElem.children),
+        progress: !!attrs['step-progress']
+    }, attrs) as ScrollamaOptions
+})
 
-      this._scroller
-        .setup(this.opts)
+onMounted(() => {
+    scroller.destroy()
+    scroller
+        .setup(opts.value)
         .onStepProgress(resp => {
-          this.$emit('step-progress', resp)
+          emit('step-progress', resp)
         })
         .onStepEnter(resp => {
-          this.$emit('step-enter', resp);
+          emit('step-enter', resp);
         })
         .onStepExit(resp => {
-          this.$emit('step-exit', resp);
-        });
+          emit('step-exit', resp);
+        })
 
-      window.addEventListener('resize', this.handleResize);
-    },
-    handleResize () {
-      this._scroller.resize();
-    }
-  }
-};
+    window.addEventListener('resize', handleResize);
+})
+
+onBeforeUnmount(() => {
+    scroller.destroy();
+})
+
+function handleResize(){
+    scroller.resize()
+}
+
+
 </script>
